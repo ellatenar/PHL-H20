dfce7c57d18fc40960c22fb484a3b703b52d6ba4
Ella Heron <heronsound95@gmail.com>
2021-07-22 00:23:40 -0400

    adds GeoJSON data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Helmet } from "react-helmet";
-// import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+import watershedData from "./static/Major_Watersheds_Regional.geojson.json";
 
 function App() {
   return (
@@ -35,20 +35,16 @@ function App() {
         </p>
       </header>
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
+        center={[40.0217, -75.2013]}
+        zoom={10}
+        scrollWheelZoom={true}
         id="mapDiv"
       >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <GeoJSON data={watershedData} />
       </MapContainer>
     </div>
   );
