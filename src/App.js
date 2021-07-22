fde827d8f21250fe6bf7957775c87403c81ab7c4
Ella Heron <heronsound95@gmail.com>
2021-07-22 04:47:07 -0400

    adds opacity slider and satellite layer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import InfoBox from "./InfoBox";
 
 function App() {
   const [info, setInfo] = useState(false);
+  const [opacity, setOpacity] = useState(0);
   const toggle = () => {
     info ? setInfo(false) : setInfo("display");
   };
@@ -40,6 +41,19 @@ function App() {
           </span>
         </p>
         <p>visualizing the watersheds of occupied Lenapehoking</p>
+        <div className="slider">
+          <span>grid </span>
+          <input
+            type="range"
+            min={0}
+            max={1}
+            step="any"
+            defaultValue={opacity}
+            onChange={(e) => setOpacity(+e.target.value)}
+            className="layerslider"
+          />
+          <span> satellite</span>
+        </div>
       </header>
       {info && <InfoBox open={info} />}
       <MapContainer
@@ -52,7 +66,14 @@ function App() {
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          opacity={1 - opacity}
         />
+        <TileLayer
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+          opacity={opacity}
+        />
+        );
         <Watersheds />
         <ZoomControl position="bottomright" />
       </MapContainer>
